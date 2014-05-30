e2eb61cea3ed991a225d8cbf6f7d6212210a9ee7
Pablo Oldani <oldani.pablo@gmail.com>
2014-05-30 16:11:10 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FarmaciasBuenosAires::Application.config.secret_key_base = '29ffaa8d98fd4790f2b85a57a343442ea8b6999cd2c4234aab292cacb3a60a0c0eb7f9662ef8a85fae7d04e44aa04a940e1e84ca9aeb140c3e35d92c71dfbc4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FarmaciasBuenosAires::Application.config.session_store :cookie_store, key: '_farmacias_buenos_aires_session'
