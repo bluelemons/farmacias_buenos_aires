4c697edd152481dcf671b3adcb4ecc8bf452a213
Eloy Espinaco <eloyesp@gmail.com>
2014-06-01 01:19:29 -0300

    Add a name for the places.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601035349) do
+ActiveRecord::Schema.define(version: 20140601041615) do
 
   create_table "locations", force: true do |t|
     t.string   "address"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140601035349) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "network_id"
+    t.string   "name"
   end
 
   add_index "places", ["location_id"], name: "index_places_on_location_id"
